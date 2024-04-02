2e744a6d43f6b19c447e5f65d9833ae3742290d2
Gayle McClure <gaylekdennison@gmail.com>
2024-04-02 21:05:20 +1100

    updated the accordion drowdown open toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//import axios from 'axios';
+
 //Selectors
 const startDateEl = $("#start-date");
 const endDateEl = $("#end-date");
@@ -22,31 +24,11 @@ if (data === null) {
 
 //--------------------Users---------------------------
 
-// //Get users from local storage
-// const getLocalUsers = () => {
-//   const users = JSON.parse(localStorage.getItem("users"));
-//   return users;
-// };
-
-// const addUsers = () => {
-//   let users = getLocalUsers();
-//   //New user object
-//   const newUser = {};
-//   //check if anything saved in local
-//   if (users === null) {
-//     users = localStorage.setItem("users", JSON.stringify([newUser]));
-//   } else if (Array.isArray(users)) {
-//     let userArr = users;
-//     userArr.push(newUser);
-//     localStorage.setItem("users", JSON.stringify(userArr));
-//   }
-// };
 
 //--------------------Trip-----------------------------
 
-// const addTrip = () => {};
 
-//-------------Dashboard
+//-------------Dashboard -----------------------------con
 
 //Calculate time from now until trip start date
 const calculateCountdown = (start) => {
@@ -89,23 +71,22 @@ const getLocalTrips = () => {
         });
       //Create the Bulma card for each trip
       if (getIcon !== undefined) {
-        const card = $("<div></div>").addClass("card");
-        const header = $("<header></header>").addClass("card-header fixed-grid has-5-cols");
-        const grid = $("<div></div>").addClass("grid");
+        const card = $("<div></div>").addClass("card fixed-grid has-5-cols");
+        const header = $("<header></header>").addClass("card-header grid");
         const name = $("<p></p>").addClass("cell card-header-title").text(trip.tripName);
         const location = $("<p></p>").addClass("cell card-header-title").text(trip.location);
         const countdown = $("<p></p>").addClass("cell card-header-title").text(countdownTime);
         const weather = $("<img />")
           .addClass("cell card-header-title")
           .attr("src", `https://openweathermap.org/img/w/${getIcon}.png`);
-        const downButton = $("<button></button>").addClass("card-header-icon").attr("id", "accordion");
-        const span = $("<span></span>").addClass("icons");
-        const icon = $("<i></i>").addClass("fas fa-angle-down has-text-black").attr("id", "accordion-icon");
-        span.append(icon);
-        downButton.append(span);
-        grid.append(name, location, countdown, weather, downButton);
-        header.append(grid);
-        card.append(header);
+        const downButton = $("<button></button>").addClass("card-header-icon").attr("id", "open-icon").attr("value", trip.id);
+        const icon = $("<i></i>").addClass("fas fa-angle-down has-text-black").attr("id", "down-icon").attr("value", trip.id);
+        const content = $('<div></div>').addClass("card-content hidden").attr("id", trip.id)
+        const testContent = $('<div></div>').addClass("content").text("soomethingkfjkjk")
+        content.append(testContent)
+        downButton.append(icon)
+        header.append(name, location, countdown, weather, downButton);
+        card.append(header, content);
         dashboardEl.append(card);
       }
     });
@@ -113,47 +94,81 @@ const getLocalTrips = () => {
 };
 
 
+//Get the distance from Distance.to API
+//TODO - figure out the inputs and call
 const calculateDistance = async () => {
-  // let origin = "Melbourne Australia";
-  // let destination = "Castlemaine Australia"
-//  const distanceApi =  `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&units=metric&key=${GOOGLE_API}`;
-let distanceApi = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=Melbourne%20Australia&destinations=Castlemaine%20Australia&units=imperial&key=AIzaSyBSGf6-LnqBBH9jGh_W2Irz3_DVhtad92s' 
-let headers = new Headers();
-headers.append('Access-Control-Allow-Origin', 'file:///C:/Users/gayle/Dev/bootcamp/travel-dashboard');
-await fetch(distanceApi, {
-  mode: "same-origin"
-})
-  .then(async function (response) {
-    console.log(response)
-    await response.json()
-  })
-  .then(function (data) {
-    console.log(data)
-    return data
+  const options = {
+    method: 'POST',
+    url: 'https://distanceto.p.rapidapi.com/distance/route',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '301d329a91msha1c6a687f656696p1b5bb9jsnff59ba98ffff',
+      'X-RapidAPI-Host': 'distanceto.p.rapidapi.com'
+    },
+    data: {
+      route: [
+        {
+          country: 'AUS',
+          name: 'Castlemaine'
+        },
+        {
+          country: 'AUS',
+          name: 'Melbourne'
+        }
+      ]
+    }
+  };
+  
+  try {
+    const response = await axios.request(options);
+    let time = response.data.route.car.duration;
+    let distance = response.data.route.car.distance;
+    const travel = {
+      time: time,
+      distance: distance
 
-  });
+    }
+    // console.log(calculateTime(time))
 
-};
-
-// "No 'Access-Control-Allow-Origin'
-
-
-const getWeather = async (lat, lon) => {
-  if (lat !== undefined && lon !== undefined) {
-    let todayArr = [];
-    let forecastArr = [];
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+    return travel
+  } catch (error) {
+    console.error(error);
   }
-};
+}
 
-calculateDistance()
+//Calculate the travel time
+const calculateTime = (seconds) => {
+  const time = Number(seconds);
+  const calcHour = Math.floor(time / 3600);
+  const calcMinutes = Math.floor(time % 3600 / 60);
+
+  const hour = calcHour > 0 ? calcHour + (calcHour == 1 ? " hour, " : " hours, ") : "";
+  var minutes = calcMinutes > 0 ? calcMinutes + (calcMinutes == 1 ? " minute, " : " minutes, ") : "";
+  return hour + minutes
+}
+
+// calculateDistance()
+
+
+
+
 //----------Event Handlers---------------
 
 //Add in travel button handler
 getLocalTrips();
 
 
-//Dashboard - opening the accordion dropdown
-$(".card-header-icon").on("click", (e) => {
-  console.log(e);
-});
+//Event handler for opening the accordion. Checks if main button is clicked, or just the icon
+$('#data').on("click",function(e) {
+    if(e.target.id === "open-icon") {
+      const cardData = $(`#${e.target.value}`);
+      cardData.toggleClass("hidden")
+      console.log(e)
+
+    } 
+    else if (e.target.id === "down-icon") {
+      const iconData = $(`#${e.target.attributes[2].value}`)
+      iconData.toggleClass("hidden")
+    }
+  });
+
